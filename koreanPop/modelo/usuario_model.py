61f1dd8a2a5499fede6d85b432723d69805b245c
Sebastián Aranda Ovallle <sebastian.aranda.o@gmail.com>
2024-05-07 11:11:22 -0400

    Update usuario_model.py

--- koreanPop/modelo/usuario_model.py
+++ koreanPop/modelo/usuario_model.py
@@ -3,7 +3,7 @@ from typing import Optional
 from sqlmodel import Field
 
 
-class Usuario():
+class Usuario(rx.Model, table=True):
     ID: Optional[int]=Field(default=None, primary_key=True)
     Nombre: str
     Email: str
